6d559af8b4e1ba8e12442de0be3765711da467e1
Eduard <barned85@gmail.com>
2025-07-13 00:32:33 +0300

    all

--- config/settings.py
+++ config/settings.py
@@ -37,22 +37,20 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap.bundle.min.js",
     "image_cropping",
     "phonenumber_field",
-    "django_celery_beat",
     "widget_tweaks",
     "restaurant",
     "users",
-    'bootstrap'
-    ]
+    "phonenumbers"
+)
 
 
 MIDDLEWARE = [
